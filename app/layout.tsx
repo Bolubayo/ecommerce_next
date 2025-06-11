4b3a59b0a7823a155136ee3404b310baf83fe1ad
Bolubayo <abayomi.ogunmade@stu.cu.edu.ng>
2025-06-11 15:47:07 +0100

    fourth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/footer/Footer";
 import NavBarContainer from "@/components/navbar/NavBarContainer";
 import { ToastContainer } from 'react-toastify';
 import { CartProvider } from "@/context/CartContext";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const ubuntu = Ubuntu({
@@ -32,6 +33,7 @@ export default function RootLayout({
             <NavBarContainer />
             <ToastContainer />
             {children}
+            <Analytics />
             <Footer />
           </CartProvider>
         </main>
